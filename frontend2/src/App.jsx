284e6add95fb5dd0a9ec15fa4f487ef93ca70a81
Ghrado <ghrado@gmail.com>
2026-01-29 23:30:45 +0200

    category commit/50

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/hero.jsx';
+import Category from "./components/Category/Category.jsx";
 
 const App = () => {
   return <div>
     <Navbar/>
     <Hero/>
+    <Category/>
   </div>;
 };
 
--- frontend2/src/components/Hero/hero.jsx
+++ frontend2/src/components/Hero/hero.jsx
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import Image1 from '../../assets/hero/headphone.png'
 import Image2 from '../../assets/category/vr.png'
 import Image3 from '../../assets/category/macbook.png'
-
+import Button from '../Shared/Button';
 
 const HeroData = [
     {
@@ -11,7 +11,7 @@ const HeroData = [
         img: Image1,
         subtitle: 'Beats Headphones',
         title: 'Solo Pro Wireless',
-        title2: 'Noise Cancelling',
+        title2: 'Confort',
 
     },
     {
@@ -19,7 +19,7 @@ const HeroData = [
         img: Image2,
         subtitle: 'Beats Headphones',
         title: 'Solo Pro Wireless',
-        title2: 'Sport Red',
+        title2: 'Sport',
 
     },
     {
@@ -46,31 +46,34 @@ const Hero = () => {
   return (
     <div className='container'>
         <div className='overflow-hidden rounded-3xl min-h-[550px]
-        sm:min-h-[650px] hero-bg-color'>
+        sm:min-h-[650px] hero-bg-color flex justify-center items-center'>
             <div className='container pb-8 sm:pb-0'>
             {/* Hero content goes here */}
 
             <Slider {...settings}>
-                
                 {HeroData.map((data) => (
                     <div key={data.id}>
                         <div className='grid grid-cols-1 sm:grid-cols-2'>
                             {/*Text Content section*/}
-                            <div>
-                                <h1>{data.subtitle}</h1>
-                                <h1>{data.title}</h1>
-                                <h1>{data.title2}</h1>
+                            <div className='flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
+                                <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold'>{data.subtitle}</h1>
+                                <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
+                                <h1 className='text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold'>{data.title2}</h1>
                                 <div>
-                                    <button>Shop Now</button>
+                                    <Button 
+                                    text="Shop by Category" 
+                                    bgColor="bg-primary" 
+                                    textColor="text-white" 
+                                    />
                                 </div>
                             </div>
                             {/*Img Content section*/}
-                            <div>
+                            <div className='order-1 sm:order-2'>
                                 <div>
                                     <img src={data.img} alt='' 
-                                    className='w-[300px] h-[300px] sm:h-[450px]
+                                    className='w-[300px] sm:w-[450px] h-[300px] sm:h-[450px]
                                     sm:scale-105 lg:scale-110 object-contain 
-                                    mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,0.4)]'
+                                    mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,0.4)] relative z-40'
                                     />
                                 </div>
                             </div>
