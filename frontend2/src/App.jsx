61e7b9f92ee76fbcfedc1ccf1ad23b69ac25db7d
Ghrado <ghrado@gmail.com>
2026-01-30 16:40:35 +0200

    services inicial commit

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/hero.jsx';
 import Category from "./components/Category/Category.jsx";
+import Category2 from "./components/Category/Category2.jsx";
+import Services from "./assets/Services/Services.jsx";
 
 const App = () => {
-  return <div>
+  return <div className="bg-white dark:bg-gray-900 dark:text-white
+  duration-200 overflow-hidden">
     <Navbar/>
     <Hero/>
     <Category/>
+    <Category2/>
+    <Services/>
   </div>;
 };
 
