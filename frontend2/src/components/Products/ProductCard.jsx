bebcb1d3613b672ee8c16c74b3e541081b7fc9fa
Ghrado <ghrado@gmail.com>
2026-02-01 00:05:14 +0200

    products commit 95

--- frontend2/src/components/Products/ProductCard.jsx
+++ frontend2/src/components/Products/ProductCard.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import Button from '../Shared/Button'
 
 const ProductCard = ({data}) => {
   return (
     <div className='mb-10'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 place-items-center'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 
+        md:grid-cols-4 gap-5 place-items-center'>
             {/* Product Cards */}
             {data.map((data) => (
-                <div key={data.id}>
-                    <div>
+                <div className='group' key={data.id}>
+                    <div className='relative'>
                         <img src={data.img}
-                        className='[h-180px] w-[260px] object-cover rounded-md'
+                        className='h-[180px] w-[260px] object-cover rounded-md'
                         />
+                        {/* hover Button */}
+                        <div className='hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 
+                        -translate-x-1/2 h-full w-full text-center 
+                        group-hover:backdrop-blur-sm justify-center items-center duration-200'>
+                            <Button
+                            text={'Add to Cart'}
+                            bgColor={'bg-primary'}
+                            textColor={'text-white'}
+                            />
+                        </div>
                     </div>
                     <div className='leading-7'>
                         <h2 className='font-semibold'>{data.title}</h2>
