96fc0bbbee01cf00cddd2b63e2de3edb43cbfd79
Ghrado <ghrado@gmail.com>
2026-01-31 23:27:07 +0200

    products commit 50

--- frontend2/src/components/Products/ProductCard.jsx
+++ frontend2/src/components/Products/ProductCard.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const ProductCard = ({data}) => {
   return (
-    <div>ProductCard</div>
+    <div className='mb-10'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 place-items-center'>
+            {/* Product Cards */}
+            {data.map((data) => (
+                <div key={data.id}>
+                    <div>
+                        <img src={data.img}
+                        className='[h-180px] w-[260px] object-cover rounded-md'
+                        />
+                    </div>
+                    <div className='leading-7'>
+                        <h2 className='font-semibold'>{data.title}</h2>
+                        <h2 className='font-bold'>{data.price}</h2>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
