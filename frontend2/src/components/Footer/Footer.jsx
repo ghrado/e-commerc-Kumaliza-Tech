04d50731d5c8dd27714aa8c398873895236f11a9
Ghrado <ghrado@gmail.com>
2026-02-02 17:31:45 +0200

    footer commit 2

--- frontend2/src/components/Footer/Footer.jsx
+++ frontend2/src/components/Footer/Footer.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        
+    <div className='dark:bg-gray-950'>
+        <div className="container">
+          <div className="grid md:grid-cols-3 pb-20 pt-5">
+            {/*Detalhes da Empresa*/}
+            <div className='py-8 px-4'>
+              <a 
+                    href="#"
+                    className='text-primary 
+                    font-semibold tracking-widest
+                    text-2xl uppercase sm:text-3xl'>
+                        Kumaliza Tech
+              </a>
+              <p className='text-gray-600 mt-4'>
+                Kumaliza Tech da incubadora de negocios da UEM é uma startup focada em fornecer solucoes tecnologicas inovadoras
+              </p>
+              <p className='text-gray-500'>
+                 Made with ❤️ by Ghrado 2026
+              </p>
+            </div>
+          </div>
+        </div>   
     </div>
   )
 }
